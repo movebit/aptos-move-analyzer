d35c128aedb25a6ebcca13c5b54b010f12459940
robin <robin@bitslab.xyz>
2024-09-23 13:42:41 +0800

    fix issue#11: fixed panic bug when called StructEnv::get_variants()

--- src/goto_definition.rs
+++ src/goto_definition.rs
@@ -797,20 +797,19 @@ impl Handler {
 
         let mut field_env_vec = target_struct.get_fields().collect_vec();
         // process enum
-        for enum_field in target_struct.get_variants() {
-            // for variant_attr in target_struct.get_variant_attributes(enum_field) {
-            //     log::info!("variant_attr.name = {:?}", variant_attr.name());
-            // }
-            let variant_loc = target_struct.get_variant_loc(enum_field);
-            if variant_loc.span().start() <= atomic_field_loc.span().start()
-                && atomic_field_loc.span().end() <= variant_loc.span().end()
-            {
-                let variant_str = env.get_source(&variant_loc);
-                log::info!("variant_str = {:?}", variant_str);
-                for field_env in target_struct.get_fields_of_variant(enum_field) {
-                    field_env_vec.push(field_env);
+        if target_struct.has_variants() {
+            for enum_field in target_struct.get_variants() {
+                let variant_loc = target_struct.get_variant_loc(enum_field);
+                if variant_loc.span().start() <= atomic_field_loc.span().start()
+                    && atomic_field_loc.span().end() <= variant_loc.span().end()
+                {
+                    let variant_str = env.get_source(&variant_loc);
+                    log::info!("variant_str = {:?}", variant_str);
+                    for field_env in target_struct.get_fields_of_variant(enum_field) {
+                        field_env_vec.push(field_env);
+                    }
+                    break;
                 }
-                break;
             }
         }
 
